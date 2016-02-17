f800e4b6dd97e86f7c2dd904693eda23df516730
Oleg Sulyanov <oleg@sulyanov.com>
2016-02-17 22:50:45 +0700

    Request responce in JSON format on GET request

--- lib/metrika/helpers/request.rb
+++ lib/metrika/helpers/request.rb
@@ -12,7 +12,7 @@ module Metrika
 
       def get(path, params = {}, options = {})      
         begin
-          response = self.token.get(path, DEFAULT_OPTIONS.merge(:params => params).merge(options))
+          response = self.token.get("#{path}.json", DEFAULT_OPTIONS.merge(:params => params).merge(options))
         rescue OAuth2::Error => e            
           self.process_oauth2_errors(e.response.status, e.message, path, params)
         end  
